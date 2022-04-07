857e52f6fc77b5d9a1819614667773aa7719df12
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-04-07 17:07:11 -0400

    unfinished implementation of page table and physical memory

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef unsigned char BYTE;
 
+#define MASK_VALID_INVALID_BIT      ((unsigned int)0x80000000)
 #define BACKING_STORE_FILENAME      "BACKING_STORE.bin"
 #define LOGICAL_ADDRESSES_FILENAME  "addresses.txt"
 #define BUFFER_LEN                  256
@@ -11,45 +11,41 @@ typedef unsigned char BYTE;
 #define NUM_PHYS_MEM_ENTRY          (NUM_PAGE_TABLE_ENTRY)
 #define PAGE_SIZE                   256                 // 2^8 in bytes
 #define EMPTY_PAGE_ENTRY            0
-#define VALID_PAGE_ENTRY            1
-#define INVALID_PAGE_ENTRY          0
+#define VALID_PAGE_ENTRY            0x80000000
+#define INVALID_PAGE_ENTRY          0x00000000
 #define FRAME_SIZE                  (PAGE_SIZE)
 #define NUM_TLB_TABLE_ENTRY         16
+#define BACKING_STORE_SIZE          ((unsigned int)(0x10000))
 
 
 int logical_addresses_size = 1;
 unsigned int *logical_addresses;
-unsigned int **page_table;
+unsigned int page_table[NUM_PAGE_TABLE_ENTRY];
+unsigned char *physical_memory;
 
 void read_addresses(const char *filename, int *const size,unsigned int *logical_addresses);
 int get_page_number(int bin_val);
 int get_offset(int bin_val);
-unsigned int **init_page_table(unsigned int **tbl);
-unsigned char *physical_memory[FRAME_SIZE];
+unsigned int *init_page_table(unsigned int *tbl);
+void swap_in(unsigned int page_num);
+// void update_page_table(unsigned int page_num, )
 
 
 
 int main(int argc, char **argv)
 {
     FILE *fd = fopen(BACKING_STORE_FILENAME, "rb");
-    for (int i = 0; i < NUM_PHYS_MEM_ENTRY; i++)
-    {
-        physical_memory[i] = (char *) malloc(sizeof(char));
-    }
+
+    // initialize page table
     init_page_table(page_table);
-    // return 0;
+    
+    // read logical address from addresses.txt
     logical_addresses = (unsigned int *) malloc(logical_addresses_size * sizeof(unsigned int));
     read_addresses(LOGICAL_ADDRESSES_FILENAME, &logical_addresses_size, logical_addresses);
 
-    printf("%d %d %d\n",logical_addresses[2], get_page_number(logical_addresses[2]), get_offset(logical_addresses[2]));
+    printf("%d, %d\n", get_page_number(logical_addresses[0]), get_offset(logical_addresses[0]));
+    return 0;
 
-    
-    // for (int y = 0; y < NUM_PAGE_TABLE_ENTRY; y++)
-    // {
-    //     fseek(fd, get_page_number(logical_addresses[y]) * FRAME_SIZE, SEEK_SET);
-    //     fread(physical_memory[get_page_number(logical_addresses[y])], sizeof(char), FRAME_SIZE, fd);
-    //     printf("%d %s\n",y, physical_memory[get_page_number(logical_addresses[y])]);
-    // }
     free(logical_addresses);
     return 0;
 }
@@ -63,7 +59,7 @@ void read_addresses(const char *filename, int *const size, unsigned int *arr)
     {
         arr = (unsigned int *) realloc(arr, (curr_index + 1) * sizeof(unsigned int));
         *(arr + curr_index) = (unsigned int) atoi(buffer);
-        printf("%d = %d\n", curr_index, arr[curr_index]);
+        // printf("%d = %d\n", curr_index, arr[curr_index]);
         memset(buffer, 0, BUFFER_LEN);
         curr_index++;
     }
@@ -73,40 +69,68 @@ void read_addresses(const char *filename, int *const size, unsigned int *arr)
 
 int get_page_number(int bin_val)
 {
-    // logical address binary format
-    // 32-bit integer numbers represents logical addresses
-    // 00000000 00000000 (00000000) 00000000
-    //                    page num
+    /*
+     *  logical address binary format
+     *  32-bit integer numbers represents logical addresses
+     *  00000000 00000000 (00000000) 00000000
+     *                     page num
+     */
 
     // shift 8 bits to the right and mask the first 8 bits
     return ((bin_val >> 8) & 0xff);
 }
 int get_offset(int bin_val)
 {
-    // logical address binary format
-    // 32-bit integer numbers represents logical addresses
-    // 00000000 00000000 00000000 (00000000)
-    //                             offset
+    /*
+     *  logical address binary format
+     *  32-bit integer numbers represents logical addresses
+     *  00000000 00000000 00000000 (00000000)
+     *                              offset
+     */                
     
     // mask first 8 bits
     return (bin_val & 0xff);
 }
 
-unsigned int **init_page_table(unsigned int **tbl)
+unsigned int *init_page_table(unsigned int *tbl)
 {
+    /*
+     *  page table contains unsigned integer (32-bit)
+     *  most left bit represent the validity of the page entry
+     *  valid page entry represented by     10000000 00000000 00000000 00000000 (binary)
+     *                                      0x80000000                          (hexadecimal)
+     *  invalid page entry represented by   00000000 00000000 00000000 00000000 (binary)
+     *                                      0x00000000                          (hexadecimal)
+     */ 
     if (!tbl) {
-        tbl = (unsigned int **) calloc(NUM_PAGE_TABLE_ENTRY, sizeof(unsigned int*));
+        tbl = (unsigned int *) calloc(NUM_PAGE_TABLE_ENTRY, sizeof(unsigned int*));
         for (int i = 0; i < NUM_PAGE_TABLE_ENTRY; i++)
         {
-            tbl[i] = (unsigned int *) calloc(2, sizeof(unsigned int));
-            tbl[i][0] = EMPTY_PAGE_ENTRY;
-            tbl[i][1] = INVALID_PAGE_ENTRY;
+            tbl[i] = 0 | VALID_PAGE_ENTRY;
+        }
+    } else {
+        for (int i = 0; i < NUM_PAGE_TABLE_ENTRY; i++)
+        {
+            tbl[i] = 0 | VALID_PAGE_ENTRY;
         }
     }
 
     // for (int x = 0; x < NUM_PAGE_TABLE_ENTRY; x++)
-    //     {
-    //         printf("%d %d %d\n", x, tbl[x][0], tbl[x][1]);
-    //     }
+    // {
+    //     printf("%08x \n", tbl[x]);
+    // }
     return tbl;
+}
+
+void swap_in(unsigned int page_num)
+{
+    FILE *fd = fopen(BACKING_STORE_FILENAME, "rb");
+    char buffer[FRAME_SIZE];
+    fseek(fd, page_num * FRAME_SIZE, SEEK_SET);
+    int read_;
+    fread(buffer, sizeof(unsigned char), FRAME_SIZE, fd);
+    if (!physical_memory[page_num])
+        physical_memory[page_num] = (unsigned char *) calloc(FRAME_SIZE, sizeof(unsigned char));
+    strncpy(physical_memory[page_num], buffer, FRAME_SIZE);
+    fclose(fd);
 }
--- test01.c
+++ test01.c
@@ -1,6 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
 
+#define PAGE_SIZE 256 // in bytes
+#define FRAME_SIZE (PAGE_SIZE) // in bytes
+#define BACKING_STORE_SIZE (pow(2, 16)) // in bytes
+
+static char *physical_memory[FRAME_SIZE];
+
+void display_page(int page_num);
 
 int main()
 {
@@ -21,30 +30,48 @@ int main()
     // printf("%s\n", buffer);
     // printf("%c\n", buffer[0]);
 
-    char *buffer;
-    buffer = (char *) calloc(256, sizeof(char));
-    int read = 0;
-    int i;
-    int addr = 0x000000;
-    read = fread(buffer, sizeof(char), 256, fd);
-    if (read > 0) {
-        printf("read = %d\n", read);
-        for(i = 0; i < read; i++)
-        {
-            if (buffer[i] == 0 || buffer[i] == NULL || buffer[i] == '\0')
-                printf("%c ", buffer[i]);
-            else
-                printf("%c ", buffer[i]);
-            // if (((i+1) % 0x10) == 0) {
-            //     printf("\n");
+    for (int i = 0; i < (BACKING_STORE_SIZE / FRAME_SIZE); i++)
+    {
+        char buffer[FRAME_SIZE];
+        fseek(fd, i * FRAME_SIZE, SEEK_SET);
+        int read = fread(buffer, sizeof(char), FRAME_SIZE, fd);
+        physical_memory[i] = (char *) calloc(FRAME_SIZE, sizeof(char));
+        if (read > 0) {
+            strncpy(physical_memory[i], buffer, FRAME_SIZE);
+            // for (int x = 0; x < FRAME_SIZE; x++)
+            // {
+            //     physical_memory[i][x] = buffer[x];
             // }
         }
-        printf("\n");
-        // printf("%s\n", buffer);
-    } else {
-        printf("Error occured while reading from .bin file\n");
-        return 1;
+        memset(buffer, 0, FRAME_SIZE);
     }
+    display_page(0);
+
+
+    // char *buffer;
+    // buffer = (char *) calloc(256, sizeof(char));
+    // int read = 0;
+    // int i;
+    // int addr = 0x000000;
+    // read = fread(buffer, sizeof(char), 256, fd);
+    // if (read > 0) {
+    //     printf("read = %d\n", read);
+    //     for(i = 0; i < read; i++)
+    //     {
+    //         if (buffer[i] == 0 || buffer[i] == NULL || buffer[i] == '\0')
+    //             printf("%c ", buffer[i]);
+    //         else
+    //             printf("%c ", buffer[i]);
+    //         // if (((i+1) % 0x10) == 0) {
+    //         //     printf("\n");
+    //         // }
+    //     }
+    //     printf("\n");
+    //     // printf("%s\n", buffer);
+    // } else {
+    //     printf("Error occured while reading from .bin file\n");
+    //     return 1;
+    // }
     // fseek(fd, 0x80, SEEK_SET);
 
     // while((read = fread(buffer, 1, 256, fd)) > 0)
@@ -62,4 +89,13 @@ int main()
     // }
 
     return 0;
+}
+
+void display_page(int page_num)
+{
+    for (int i = 0; i < PAGE_SIZE; i++)
+    {
+        printf("%02x ", physical_memory[page_num][i]);
+    }
+    printf("\n");
 }
